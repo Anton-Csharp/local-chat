7ec448cd3af6fb65c389d13e9f2a6199fa760917
Anton-Csharp <AntonCheh456@gmail.com>
2025-06-12 17:19:13 +0300

    Добавьте файлы проекта.

---
+++ local chat/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace local_chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtmsg = new System.Windows.Forms.TextBox();
+            this.listUsers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtchat = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtmsg
+            // 
+            this.txtmsg.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtmsg.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtmsg.Location = new System.Drawing.Point(264, 470);
+            this.txtmsg.Multiline = true;
+            this.txtmsg.Name = "txtmsg";
+            this.txtmsg.Size = new System.Drawing.Size(305, 34);
+            this.txtmsg.TabIndex = 0;
+            // 
+            // listUsers
+            // 
+            this.listUsers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.listUsers.FormattingEnabled = true;
+            this.listUsers.Location = new System.Drawing.Point(12, 58);
+            this.listUsers.Name = "listUsers";
+            this.listUsers.Size = new System.Drawing.Size(246, 446);
+            this.listUsers.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "стикеры";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(69, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 36);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "telegram";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::local_chat.Properties.Resources.загрузка__2_;
+            this.pictureBox3.Location = new System.Drawing.Point(519, 396);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(102, 68);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::local_chat.Properties.Resources.загрузка__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(419, 396);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(94, 68);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::local_chat.Properties.Resources.загрузка;
+            this.pictureBox1.Location = new System.Drawing.Point(320, 396);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(93, 68);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackgroundImage = global::local_chat.Properties.Resources.OIP;
+            this.btnSend.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSend.Location = new System.Drawing.Point(575, 470);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(46, 34);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtchat
+            // 
+            this.txtchat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtchat.Location = new System.Drawing.Point(301, 58);
+            this.txtchat.Multiline = true;
+            this.txtchat.Name = "txtchat";
+            this.txtchat.Size = new System.Drawing.Size(320, 332);
+            this.txtchat.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(644, 516);
+            this.Controls.Add(this.txtchat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listUsers);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtmsg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtmsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox listUsers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtchat;
+    }
+}
+
---
+++ local chat/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace local_chat
+{
+    public partial class Form1 : Form
+    {
+        const string MulticastAddress = "224.0.0.224";
+        const int Port = 5000;
+        const int HelloInterval = 5000;
+        const string MessagePrefix = "MSG:";
+        const string HelooPrefix = "HELLO:";
+        private UdpClient udpClient;
+        private IPEndPoint groupEP;
+        private Thread receiveThread;
+        private bool isRunning;
+        private readonly string userName = Environment.MachineName;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeNetwork();
+            SetupUI();
+            Startlistening();
+        }
+        private void InitializeNetwork()
+        {
+            try
+            {
+                udpClient = new UdpClient();
+                udpClient.Client.SetSocketOption(
+                SocketOptionLevel.Socket,
+                SocketOptionName.ReuseAddress,
+                true
+                );
+                udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, Port));
+                udpClient.JoinMulticastGroup(IPAddress.Parse(MulticastAddress));
+                groupEP = new IPEndPoint(IPAddress.Parse(MulticastAddress),Port );
+                isRunning = true;
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show($"ошибка{ex.Message}");
+                Close();
+            }
+        }
+        private void SetupUI()
+        {
+            Text = $"локальный чат:{userName}";
+            FormClosing += Form1_FormClosing;
+            btnSend.Click += btnSend_Click;
+            var timer  =  new System.Windows.Forms.Timer { Interval = HelloInterval};
+            timer.Tick += (s, e) => SendMessage(HelooPrefix + userName);
+            timer.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isRunning = false ;
+            udpClient?.Close();
+            receiveThread ?.Join(1000);
+        }
+       
+        private void SendMessage(string message)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                udpClient.Send(data, data.Length, groupEP);
+            }
+            catch(Exception ex)
+            {
+                txtchat.AppendText($"Ошибка отправки:{ex.Message}\r\n");
+            }
+        }
+        private void ProcessReceivedMessage(string message,IPAddress senderIp)
+        {
+            if (message.StartsWith(HelooPrefix))
+            {
+                string name = message.Substring(HelooPrefix.Length);
+                if (!listUsers.Items.Contains(name)
+                   && name != userName)
+                {
+                    listUsers.Items.Add(name);
+                    txtchat.AppendText($"[{name} присоединился]\r\n");
+                }
+            }
+            else if (message.StartsWith(MessagePrefix));
+            {
+                string text = message.Substring (MessagePrefix.Length);
+                txtchat.AppendText($"{text}\r\n");
+            }
+
+        }
+        private void Startlistening()
+        {
+            receiveThread = new Thread(() =>
+            {
+                try
+                {
+                    while (isRunning)
+                    {
+                        IPEndPoint remoteEP = null;
+                        byte[] data = udpClient.Receive(ref remoteEP);
+                        string message = Encoding.UTF8.GetString(data);
+                        this.Invoke((MethodInvoker)delegate{  
+                             ProcessReceivedMessage(message, remoteEP.Address);
+                        });
+
+                    }
+                }
+                catch (SocketException) { }
+
+
+
+                
+            })
+            { IsBackground = true };
+            receiveThread.Start();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string text = txtmsg.Text.Trim();
+            if (!string.IsNullOrEmpty(text))
+            {
+                SendMessage($"{MessagePrefix}{userName}:{text}");
+                txtmsg.Clear();
+            }
+        }
+    }
+}
